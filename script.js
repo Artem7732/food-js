c3643a587bc5df57cb5a12713cc2d6fe2d4e8a79
artem <artem7732@gmail.com>
2022-11-05 20:42:33 +0200

    modal

--- script.js
+++ script.js
@@ -69,4 +69,36 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
     setClock('.timer', timeEnd);
+    //modal
+
+    const modalTriger = document.querySelectorAll('[data-modal]'),
+        modal = document.querySelector('.modal'),
+        modalClose = document.querySelector('[data-close]');
+    modalTriger.forEach(btn => {
+        btn.addEventListener('click', () => {
+            modal.classList.add('show');
+            modal.classList.remove('hide');
+            document.body.style.overflow = 'hidden';
+        });
+    })
+    modalClose.addEventListener('click', () => {
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        document.body.style.overflow = '';
+    })
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            modal.classList.add('hide');
+            modal.classList.remove('show');
+            document.body.style.overflow = '';
+
+        }
+    })
+    document.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape') {
+            modal.classList.remove('show');
+            document.body.style.overflow = '';
+        }
+    })
+
 });
