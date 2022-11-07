8d9483b5091ecfd2fd444737267bfb1a9205abb1
artem <artem7732@gmail.com>
2022-11-07 20:39:21 +0200

    slide

--- script.js
+++ script.js
@@ -102,10 +102,10 @@ window.addEventListener('DOMContentLoaded', () => {
         })
         //slides
 
-    const slides = document.querySelectorAll('.offer__slider-'),
+    const slides = document.querySelectorAll('.offer__slide'),
         prev = document.querySelector('.offer__slider-prev'),
-        next = document.querySelector('.offer__slider-next');
-    total = document.querySelector('#total'),
+        next = document.querySelector('.offer__slider-next'),
+        total = document.querySelector('#total'),
         current = document.querySelector('#current');
 
     let slideIndex = 1;
@@ -113,7 +113,7 @@ window.addEventListener('DOMContentLoaded', () => {
     showSlides(slideIndex);
 
     if (slides.length < 10) {
-        total.textContent = '0${slides.length}';
+        total.textContent = `0${slides.length}`;
     } else {
         total.textContent = slides.length;
     }
@@ -128,7 +128,7 @@ window.addEventListener('DOMContentLoaded', () => {
         slides.forEach(item => item.style.display = 'none');
         slides[slideIndex - 1].style.display = 'block';
         if (slides.length < 10) {
-            current.textContent = '0${slideIndex}';
+            current.textContent = `0${slideIndex}`;
         } else {
             current.textContent = slideIndex;
         }
